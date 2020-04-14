c5fc9af35eb182af669bf1ce264fd95f4b66adf7
nzupancic <nik.zupancic1@gmail.com>
2020-04-14 12:45:26 +0200

    Writing laz to file

--- bridge_reconstructor.py
+++ bridge_reconstructor.py
@@ -1,10 +1,10 @@
-from read_laz import LazReader
+from read_laz import LazManipulator
 from read_shapefile import ShapefileReader
 
 class BridgeReconstructor:
 
     def __init__(self, laz_file, shp_file):
-        laz = LazReader(laz_file)
+        laz = LazManipulator(laz_file)
         shp = ShapefileReader(shp_file)
         bridges = shp.bridgesInsideCoords(laz.x_min, laz.y_min, laz.x_max, laz.y_max)
         print("Found", len(bridges), "relevant bridges")
--- read_laz.py
+++ read_laz.py
@@ -1,19 +1,31 @@
 import pylas
+import numpy as np
+# Lazpef >= 1.3 required for writing .laz file
 
-
-class LazReader:
+class LazManipulator:
 
     def __init__(self, filename):
         fh = pylas.open(filename)
-        laz = fh.read()
-        self.x_min = laz.x.min()
-        self.x_max = laz.x.max()
-        self.y_min = laz.y.min()
-        self.y_max = laz.y.max()
-        self.points = list(zip(laz.x, laz.y))
+        self.laz = fh.read()
+        self.x_min = self.laz.x.min()
+        self.x_max = self.laz.x.max()
+        self.y_min = self.laz.y.min()
+        self.y_max = self.laz.y.max()
+        #self.points = list(zip(self.laz.x, self.laz.y))
+        self.points = self.laz.points
+        self.dt = self.points.dtype
         # print(self.points)
         # print("X min ", self.x_min, "X max", self.x_max, "Y min", self.y_min, "Y max", self.y_max)
 
+    # Adds new points to the point cloud and writes it to an output file
+    # Expects new_points to be a nested list where each entry/line consists of 10 parameters:
+    # [x, y, z, intensity, bit_fiels, classification, scan_angle_rank, user_data, point_source_id, gps_time]
+    def writeListToFile(self, new_points, name="output.laz"):
+        for entry in new_points:
+            new_point = np.array(tuple(entry), dtype=self.dt)
+            self.points = np.append(self.points, new_point)
+        self.laz.points = self.points
+        self.laz.write(name)
 
 if __name__ == "__main__":
-    LazReader("GK_430_136.laz")
+    LazManipulator("GK_430_136.laz")
