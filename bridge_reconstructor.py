3c165910ef3eb23090805ead291ec8e557fdb262
nzupancic <nik.zupancic1@gmail.com>
2020-04-24 12:20:52 +0200

    Added additional point filtering from laz

--- bridge_reconstructor.py
+++ bridge_reconstructor.py
@@ -10,15 +10,34 @@ class BridgeReconstructor:
         self.laz = LazManipulator(laz_file)
         shp = ShapefileReader(shp_file)
         self.bridges = shp.bridgesInsideCoords(self.laz.x_min, self.laz.y_min, self.laz.x_max, self.laz.y_max)
+        self.points = self.getRelevantLazPoints(self.bridges, self.laz.laz  )
+
+    def getRelevantLazPoints(self, bridges, laz):
+        result = []
+        for i in bridges:
+            x_min = i.shape.bbox[0]
+            y_min = i.shape.bbox[1]
+            x_max = i.shape.bbox[2]
+            y_max = i.shape.bbox[3]
+            current_points = laz.points[np.logical_and(np.logical_and(laz.x >= x_min, laz.x <= x_max),
+                                                       np.logical_and(laz.y >= y_min, laz.y <= y_max))]
+            print("For bridge found", len(current_points), "points")
+            result.append(current_points)
+        return result
 
     def reconstructIt(self):
-        step_size = 2
+        step_size = 20
         added_points = []
-        for bridge in self.bridges:
+        for _i in range(len(self.bridges)):
+            bridge = self.bridges[_i]
             print("Reconstructing new one")
             width = bridge.record["SIRCES"] * 100
+            first_point = [i * 100 for i in bridge.shape.points[0]]
+            last_point = [i * 100 for i in bridge.shape.points[-1]]
+            length = math.sqrt((first_point[0] - last_point[0]) * (first_point[0] - last_point[0]) +
+                               (first_point[1] - last_point[1]) * (first_point[1] - last_point[1]))
+            thickness = math.ceil(length / 8)
             for i in range(len(bridge.shape.points) - 1):
-                print("BLLALAAL")
                 point_a = list(bridge.shape.points[i]) + [bridge.shape.z[i]]
                 point_a = [x * 100 for x in point_a]
                 point_b = list(bridge.shape.points[i + 1]) + [bridge.shape.z[i + 1]]
@@ -30,11 +49,11 @@ class BridgeReconstructor:
                 for step in range(math.floor(magnitude / step_size)):
                     point_a[0] += (unit_vector[0] * step_size)
                     point_a[1] += (unit_vector[1] * step_size)
-                    print("New point a is ", point_a[0], point_a[1])
                     point_a[2] += z_step
                     point_c, point_c2 = self.findThirdPoint(point_a[0], point_a[1], point_b[0], point_b[1], width / 2)
-                    for depth in range(8):
-                        print("Adding point c", point_c[0], point_c[1])
+                    # Find relevant points at current (x,y) position and check their z value
+                    # z_limits = [pnt[2] for pnt in self.points if pnt[0] == point_c[0] and pnt[1] == point_c[1]]
+                    for depth in range(thickness):
                         added_points.append((point_c[0], point_c[1], point_a[2] - depth, 20, 82, 65, 32, 0, 2418, 109863062.84541322))
                         added_points.append((point_c2[0], point_c2[1], point_a[2] - depth, 20, 82, 65, 32, 0, 2418, 109863062.84541322))
         print("Construction finished, writing...")
@@ -51,4 +70,4 @@ class BridgeReconstructor:
 
 if __name__ == "__main__":
     b = BridgeReconstructor("TM_462_101.laz", "TN_CESTE_L")
-    b.reconstructIt()
+    #b.reconstructIt()
